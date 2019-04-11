878c18df46fe217bbd1b154e3b3e72105cd5e143
Nikita <aekawit072@gmail.com>
2019-04-11 19:29:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <!-- This file allows you to host this page as a static file on Heroku -->
-<?php header( 'Location: /index.html' ) ;  ?>
+<?php
+header( 'Location: /index.html' );
+$picurl=$_GET['picurl'];
+$uri_url=$_GET['uri_url'];
+?>
